e02a056b088d1412e8788495ac0dbc7e6731ef78
Dirk Schäfer <disch1975@gmail.com>
2026-06-11 10:42:37 +0200

    Release …

---
+++ js/printHelper.js
@@ -0,0 +1,11 @@
+﻿window.printHelper = {
+    registerAfterPrint: function (dotnetRef) {
+        window.onafterprint = function () {
+            dotnetRef.invokeMethodAsync("OnAfterPrint");
+        };
+    },
+
+    print: function () {
+        window.print();
+    }
+};
